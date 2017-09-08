d479e4a3676da0534eb9565a29566c5fce1a1933
NESTOR EMILIO PEREZ SANCHEZ <ing_nestor_perez@MacBook-Air-de-NESTOR.local>
2017-09-08 10:07:57 -0500

    Cambios en usuarios seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
 			'ap_materno' => 'Sanchez',
 			'password' => \Hash::make( '123456' ),
 			'email' => 'nes64df@gmail.com',
+			'add_frases' => true,
 			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
 			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
 		]);
@@ -29,6 +30,7 @@ class UsersTableSeeder extends Seeder
 			'ap_materno' => '',
 			'password' => \Hash::make( '123456' ),
 			'email' => 'jesus@macias-group.com',
+			'add_frases' => true,
 			'created_at' => $carbon->now()->format('Y-m-d H:i:s'),
 			'updated_at' => $carbon->now()->format('Y-m-d H:i:s')
 		]);
