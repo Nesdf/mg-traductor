8078e5d60fb206b2a250e2410c0c43ab1b98db56
NESTOR EMILIO PEREZ SANCHEZ <ing.nestor.sanz@gmail.com>
2018-01-20 13:27:47 -0600

    Sea grega add_frases en modelo

--- Modules/MgPersonal/Entities/User.php
+++ Modules/MgPersonal/Entities/User.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
 	protected $table = 'users';
-    protected $fillable = ['name', 'email', 'password', 'job', 'ap_paterno', 'ap_materno', 'password', 'password','created_at', 'updated_at'];
+    protected $fillable = ['name', 'email', 'password', 'job', 'add_frases', 'ap_paterno', 'ap_materno', 'password', 'password','created_at', 'updated_at'];
 	protected $hidden = ['password'];
 }
